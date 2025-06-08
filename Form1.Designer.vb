0c8b4d7dd1436893606ee659270948ed4d26f2cb
AntipasZemba <169179206+AntipasZemba@users.noreply.github.com>
2025-06-07 20:46:35 -0400

    User interface setup

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,17 +22,373 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.radCustom = New System.Windows.Forms.RadioButton()
+        Me.radPearlized = New System.Windows.Forms.RadioButton()
+        Me.radStandard = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.chckNavigation = New System.Windows.Forms.CheckBox()
+        Me.chckLeather = New System.Windows.Forms.CheckBox()
+        Me.chckStereo = New System.Windows.Forms.CheckBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.lblAmtDue = New System.Windows.Forms.Label()
+        Me.txtTrade = New System.Windows.Forms.TextBox()
+        Me.lblSubtotal = New System.Windows.Forms.Label()
+        Me.lblTax = New System.Windows.Forms.Label()
+        Me.lblAF = New System.Windows.Forms.Label()
+        Me.txtBase = New System.Windows.Forms.TextBox()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.txtFirstNam = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label14)
+        Me.GroupBox2.Controls.Add(Me.radCustom)
+        Me.GroupBox2.Controls.Add(Me.radPearlized)
+        Me.GroupBox2.Controls.Add(Me.radStandard)
+        Me.GroupBox2.Location = New System.Drawing.Point(41, 170)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(175, 99)
+        Me.GroupBox2.TabIndex = 49
+        Me.GroupBox2.TabStop = False
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label14.Location = New System.Drawing.Point(19, 13)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(40, 13)
+        Me.Label14.TabIndex = 26
+        Me.Label14.Text = "Finish"
+        '
+        'radCustom
+        '
+        Me.radCustom.AutoSize = True
+        Me.radCustom.Location = New System.Drawing.Point(48, 75)
+        Me.radCustom.Name = "radCustom"
+        Me.radCustom.Size = New System.Drawing.Size(60, 17)
+        Me.radCustom.TabIndex = 21
+        Me.radCustom.TabStop = True
+        Me.radCustom.Text = "Custom"
+        Me.radCustom.UseVisualStyleBackColor = True
+        '
+        'radPearlized
+        '
+        Me.radPearlized.AutoSize = True
+        Me.radPearlized.Location = New System.Drawing.Point(48, 52)
+        Me.radPearlized.Name = "radPearlized"
+        Me.radPearlized.Size = New System.Drawing.Size(68, 17)
+        Me.radPearlized.TabIndex = 20
+        Me.radPearlized.TabStop = True
+        Me.radPearlized.Text = "Pearlized"
+        Me.radPearlized.UseVisualStyleBackColor = True
+        '
+        'radStandard
+        '
+        Me.radStandard.AutoSize = True
+        Me.radStandard.Checked = True
+        Me.radStandard.Location = New System.Drawing.Point(48, 29)
+        Me.radStandard.Name = "radStandard"
+        Me.radStandard.Size = New System.Drawing.Size(68, 17)
+        Me.radStandard.TabIndex = 19
+        Me.radStandard.TabStop = True
+        Me.radStandard.Text = "Standard"
+        Me.radStandard.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label13)
+        Me.GroupBox1.Controls.Add(Me.chckNavigation)
+        Me.GroupBox1.Controls.Add(Me.chckLeather)
+        Me.GroupBox1.Controls.Add(Me.chckStereo)
+        Me.GroupBox1.Location = New System.Drawing.Point(41, 51)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(176, 109)
+        Me.GroupBox1.TabIndex = 48
+        Me.GroupBox1.TabStop = False
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label13.Location = New System.Drawing.Point(19, 13)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(75, 13)
+        Me.Label13.TabIndex = 25
+        Me.Label13.Text = "Accessories"
+        '
+        'chckNavigation
+        '
+        Me.chckNavigation.AutoSize = True
+        Me.chckNavigation.Location = New System.Drawing.Point(48, 84)
+        Me.chckNavigation.Name = "chckNavigation"
+        Me.chckNavigation.Size = New System.Drawing.Size(77, 17)
+        Me.chckNavigation.TabIndex = 18
+        Me.chckNavigation.Text = "Navigation"
+        Me.chckNavigation.UseVisualStyleBackColor = True
+        '
+        'chckLeather
+        '
+        Me.chckLeather.AutoSize = True
+        Me.chckLeather.Location = New System.Drawing.Point(48, 59)
+        Me.chckLeather.Name = "chckLeather"
+        Me.chckLeather.Size = New System.Drawing.Size(97, 17)
+        Me.chckLeather.TabIndex = 17
+        Me.chckLeather.Text = "Leather Interior"
+        Me.chckLeather.UseVisualStyleBackColor = True
+        '
+        'chckStereo
+        '
+        Me.chckStereo.AutoSize = True
+        Me.chckStereo.Location = New System.Drawing.Point(48, 36)
+        Me.chckStereo.Name = "chckStereo"
+        Me.chckStereo.Size = New System.Drawing.Size(57, 17)
+        Me.chckStereo.TabIndex = 16
+        Me.chckStereo.Text = "Stereo"
+        Me.chckStereo.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.btnExit.Location = New System.Drawing.Point(358, 318)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 47
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(228, 318)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 46
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(101, 318)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalc.TabIndex = 45
+        Me.btnCalc.Text = "Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'lblAmtDue
+        '
+        Me.lblAmtDue.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblAmtDue.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAmtDue.Location = New System.Drawing.Point(400, 263)
+        Me.lblAmtDue.Name = "lblAmtDue"
+        Me.lblAmtDue.Size = New System.Drawing.Size(85, 19)
+        Me.lblAmtDue.TabIndex = 44
+        '
+        'txtTrade
+        '
+        Me.txtTrade.Location = New System.Drawing.Point(400, 235)
+        Me.txtTrade.Name = "txtTrade"
+        Me.txtTrade.Size = New System.Drawing.Size(85, 20)
+        Me.txtTrade.TabIndex = 43
+        '
+        'lblSubtotal
+        '
+        Me.lblSubtotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblSubtotal.Location = New System.Drawing.Point(401, 188)
+        Me.lblSubtotal.Name = "lblSubtotal"
+        Me.lblSubtotal.Size = New System.Drawing.Size(85, 19)
+        Me.lblSubtotal.TabIndex = 42
+        '
+        'lblTax
+        '
+        Me.lblTax.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTax.Location = New System.Drawing.Point(401, 159)
+        Me.lblTax.Name = "lblTax"
+        Me.lblTax.Size = New System.Drawing.Size(85, 19)
+        Me.lblTax.TabIndex = 41
+        '
+        'lblAF
+        '
+        Me.lblAF.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblAF.Location = New System.Drawing.Point(401, 130)
+        Me.lblAF.Name = "lblAF"
+        Me.lblAF.Size = New System.Drawing.Size(85, 19)
+        Me.lblAF.TabIndex = 40
+        '
+        'txtBase
+        '
+        Me.txtBase.Location = New System.Drawing.Point(401, 80)
+        Me.txtBase.Name = "txtBase"
+        Me.txtBase.Size = New System.Drawing.Size(84, 20)
+        Me.txtBase.TabIndex = 39
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Location = New System.Drawing.Point(401, 54)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(85, 20)
+        Me.txtLastName.TabIndex = 38
+        '
+        'txtFirstNam
+        '
+        Me.txtFirstNam.Location = New System.Drawing.Point(401, 28)
+        Me.txtFirstNam.Name = "txtFirstNam"
+        Me.txtFirstNam.Size = New System.Drawing.Size(85, 20)
+        Me.txtFirstNam.TabIndex = 37
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(254, 269)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(80, 13)
+        Me.Label8.TabIndex = 36
+        Me.Label8.Text = "Amount Due:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(254, 242)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(102, 13)
+        Me.Label7.TabIndex = 35
+        Me.Label7.Text = "Trade In Allowance:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(255, 194)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(49, 13)
+        Me.Label6.TabIndex = 34
+        Me.Label6.Text = "Subtotal:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(255, 165)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(57, 13)
+        Me.Label5.TabIndex = 33
+        Me.Label5.Text = "Sales Tax:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(255, 136)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(118, 13)
+        Me.Label4.TabIndex = 32
+        Me.Label4.Text = "Accessories and Finish:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(255, 87)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(61, 13)
+        Me.Label3.TabIndex = 31
+        Me.Label3.Text = "Base Price:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(255, 61)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 13)
+        Me.Label2.TabIndex = 30
+        Me.Label2.Text = "Last Name:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(255, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(60, 13)
+        Me.Label1.TabIndex = 29
+        Me.Label1.Text = "First Name:"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(535, 384)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.lblAmtDue)
+        Me.Controls.Add(Me.txtTrade)
+        Me.Controls.Add(Me.lblSubtotal)
+        Me.Controls.Add(Me.lblTax)
+        Me.Controls.Add(Me.lblAF)
+        Me.Controls.Add(Me.txtBase)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.txtFirstNam)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
         Me.Text = "VB Auto"
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label14 As Label
+    Friend WithEvents radCustom As RadioButton
+    Friend WithEvents radPearlized As RadioButton
+    Friend WithEvents radStandard As RadioButton
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label13 As Label
+    Friend WithEvents chckNavigation As CheckBox
+    Friend WithEvents chckLeather As CheckBox
+    Friend WithEvents chckStereo As CheckBox
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents lblAmtDue As Label
+    Friend WithEvents txtTrade As TextBox
+    Friend WithEvents lblSubtotal As Label
+    Friend WithEvents lblTax As Label
+    Friend WithEvents lblAF As Label
+    Friend WithEvents txtBase As TextBox
+    Friend WithEvents txtLastName As TextBox
+    Friend WithEvents txtFirstNam As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
 End Class
